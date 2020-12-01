29f8ef839cb71618a6c46c18336d5439c5251230
André Figueira <andre.figueira@eagleeye.com>
2020-12-01 16:32:13 +0000

    - Tweaks

--- Resources/views/mail/order-confirmation.blade.php
+++ Resources/views/mail/order-confirmation.blade.php
@@ -1,20 +1,24 @@
-<h1>Order Confirmation</h1>
+@extends('laralite::mail.layout')
 
-<p>Thank you for your order.</p>
+@section('content')
+    <h1>Order Confirmation</h1>
 
-<h3>Order Summary</h3>
+    <p>Thank you for your order.</p>
 
-<table width="100%" style="border: 1px solid #FFF;">
-    <tr>
-        <td>SKU</td>
-        <td>Quantity</td>
-        <td>Price</td>
-    </tr>
-    @foreach($form['order']->basket->products as $product)
+    <h3>Order Summary</h3>
+
+    <table width="100%" style="border: 1px solid #FFF;">
         <tr>
-            <td>{{ $product->sku }}</td>
-            <td>&times;{{ $product->quantity }}</td>
-            <td>${{ $product->price }}</td>
+            <td>SKU</td>
+            <td>Quantity</td>
+            <td>Price</td>
         </tr>
-    @endforeach
-</table>
+        @foreach($form['order']->basket->products as $product)
+            <tr>
+                <td>{{ $product->sku }}</td>
+                <td>&times;{{ $product->quantity }}</td>
+                <td>${{ number_format($product->price/100, 2, '.', '.') }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
