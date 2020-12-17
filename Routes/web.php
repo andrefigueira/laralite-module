ab53226fa98bb3283acc60e5f16d9ebbed6de5af
André Figueira <andre.figueira@eagleeye.com>
2020-12-17 10:09:56 +0000

    - Add parallax route

--- Routes/web.php
+++ Routes/web.php
@@ -83,6 +83,8 @@ Route::group(['middleware' => 'auth'], function () {
 
 Route::get('/ticket/{uuid}', 'TicketController@generateTicket');
 
+Route::get('/parallax', 'LaraliteController@parallax');
+
 /**
  * CMS catch-all route
  *
