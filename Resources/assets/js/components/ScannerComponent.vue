dfda1d7d3ed7be9a700b2c037fb32edb43131987
anjali <anjalijangir277@gmail.com>
2021-03-23 10:55:56 +0530

    -Show last scanned ticket-id even if the ticket does not exist

--- Resources/assets/js/components/ScannerComponent.vue
+++ Resources/assets/js/components/ScannerComponent.vue
@@ -2,11 +2,9 @@
     <div>
         <div class="row">
             <div class="col-12">
-                <p class="alert alert-danger" v-if="error !== ''">{{ error }}</p>
-
-                <p v-if="result" class="decode-result">Last result: <b>{{ result }}</b></p>
-
-                <qrcode-stream @decode="onDecode" @init="onInit" />
+              <p class="alert" :class="{ 'alert-danger': this.error, 'alert-success': !this.error }" v-if="message !== ''">
+                <b>TICKET ID: </b>{{ ticket_id }} <b v-if="!this.error">Visits: </b> {{ count }} <br/>{{ message }}</p>
+              <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit"></qrcode-stream>
             </div>
         </div>
     </div>
@@ -18,8 +16,12 @@ import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 export default {
     data() {
         return {
-            result: '',
-            error: ''
+          camera: 'auto',
+          result: '',
+          error: false,
+          message: '',
+          count: '',
+          ticket_id: '',
         }
     },
     components: {
@@ -27,38 +29,61 @@ export default {
         QrcodeDropZone,
         QrcodeCapture
     },
-    methods: {
-        onDecode (result) {
-            this.result = result
-            this.error = '';
-        },
-
-        async onInit (promise) {
-            try {
-                await promise
-            } catch (error) {
-                if (error.name === 'NotAllowedError') {
-                    this.error = "ERROR: you need to grant camera access permission"
-                } else if (error.name === 'NotFoundError') {
-                    this.error = "ERROR: no camera on this device"
-                } else if (error.name === 'NotSupportedError') {
-                    this.error = "ERROR: secure context required (HTTPS, localhost)"
-                } else if (error.name === 'NotReadableError') {
-                    this.error = "ERROR: is the camera already in use?"
-                } else if (error.name === 'OverconstrainedError') {
-                    this.error = "ERROR: installed cameras are not suitable"
-                } else if (error.name === 'StreamApiNotSupportedError') {
-                    this.error = "ERROR: Stream API is not supported in this browser"
-                } else if (error.name === 'InsecureContextError') {
-                    this.error = "ERROR: MUST BE ACCESSED OVER HTTPS!"
-                }
-
-                if (this.error === '') {
-                    this.error = 'Failed due to error';
-                }
-            }
+    watch: {
+      result (newValue, oldValue) {
+        if (newValue != null && newValue !== '')
+        {
+          this.result = newValue;
+          this.verifyTicket(this.result);
         }
+
+      },
+    },
+  computed: {
+    variant() {
+      return this.error ? 'danger' : 'success';
     }
+  },
+    methods: {
+        async onDecode (result) {
+          this.result = result
+          this.message = ''
+          this.error = false;
+          this.camera = 'off'
+          await this.timeout(500)
+          this.camera = 'auto'
+        },
+        verifyTicket(result) {
+          axios.get('/api/scan/ticket/' + this.result)
+          .then((response) => {
+            console.log(response.data.message)
+            this.message = response.data.message
+            this.error = false
+            this.count = response.data.ticket.visited_counts
+            this.result = ''
+            this.ticket_id = response.data.ticket.unique_id
+          }).catch(error => {
+            console.log(error.response.data.message)
+            this.message = error.response.data.message
+            this.error = true
+            this.result = ''
+            this.count = ''
+            this.ticket_id = result
+          });
+        },
+      onInit (promise) {
+        promise
+            .catch(console.error)
+      },
+      turnCameraOff () {
+        this.camera = 'off'
+      },
+      timeout (ms) {
+        return new Promise(resolve => {
+          window.setTimeout(resolve, ms)
+        })
+      }
+    },
 }
 </script>
 
