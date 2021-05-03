37150a4c311329661bfdd950703a0c464afcd8ed
anjali <anjalijangir277@gmail.com>
2021-05-03 09:31:34 +0530

    Fix Filter/search for Orders Table

--- Http/Controllers/Api/OrderController.php
+++ Http/Controllers/Api/OrderController.php
@@ -18,24 +18,25 @@ class OrderController extends Controller
 {
     public function get(Request $request)
     {
-        $orders = Order::query();
+        $orders = Order::with(['customer']);
         $perPage = $request->get('perPage', 1);
 
         if ($request->get('all') === 'true') {
             return $orders->get();
         }
 
-        if ($request->input('filter') !== 'null') {
-            $orders
-                ->where('name', 'LIKE', '%' . $request->input('filter') . '%')
-                ->orWhere('email', 'LIKE', '%' . $request->input('filter') . '%');
-        }
+      if ($request->input('filter') !== 'null' && $request->input('filter') != '') {
+        $orders->whereHas('customer', function ($q) use ($request) {
+          $q->where('name', 'LIKE', '%' . $request->input('filter') . '%')
+            ->orWhere('email', 'LIKE', '%' . $request->input('filter') . '%');
+        })->get();
+      }
 
         if ($request->input('sortBy') !== null) {
             $orders->orderBy($request->input('sortBy'), ($request->input('sortDesc') === 'true' ? 'desc' : 'asc'));
         }
 
-        return $orders->paginate($perPage);
+        return response()->json($orders->paginate($perPage));
     }
 
     public function getOne($id)
