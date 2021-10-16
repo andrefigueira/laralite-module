953f29f4250eb1ae8d43b25deabdf6b540129976
shinracoder <peter.atkins85@gmail.com>
2021-10-16 18:23:10 +0100

    Add logout route

--- Routes/api.php
+++ Routes/api.php
@@ -101,6 +101,7 @@ Route::group([], static function () {
     Route::get('/oauth/personal-access-tokens', 'Api\PersonalAccessTokenController@get');
 
     Route::post('/login', 'Api\CustomerAuthController@login');
+    Route::get('/logout', 'Api\CustomerAuthController@logout');
     Route::post('/signup', 'Api\CustomerAuthController@signup');
 //    Route::post('/form', 'Api\FormController@submit');
 });
